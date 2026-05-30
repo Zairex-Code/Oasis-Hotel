5bd77722ef946eb40455f909f778000b307ca69f
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-30 17:24:28 -0500

    feature/ HotelResponseDTO created

--- src/main/java/com/oasis_hotel/oasis_hotel/dto/hotel/HotelResponseDTO.java
+++ src/main/java/com/oasis_hotel/oasis_hotel/dto/hotel/HotelResponseDTO.java
@@ -1,5 +1,12 @@
 package com.oasis_hotel.oasis_hotel.dto.hotel;
 
-public record HotelResponseDTO() {
+public record HotelResponseDTO(
+    Long id,
+    String name,
+    String address,
+    String city,
+    Integer stars,
+    Boolean isActive
+) {
     
 }
