adaed9668b581681ab7df9e19e82a671185da31d
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-29 23:30:39 -0500

    my first commit

---
+++ src/main/java/com/oasis_hotel/oasis_hotel/OasisHotelApplication.java
@@ -0,0 +1,13 @@
+package com.oasis_hotel.oasis_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasisHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasisHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oasis_hotel/oasis_hotel/OasisHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasis_hotel.oasis_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasisHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
