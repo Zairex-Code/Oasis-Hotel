67f0a4ed6258e18be25de04023691ea9377d7556
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-31 18:25:10 -0500

    feature/ RoomService created

---
+++ src/main/java/com/oasis_hotel/oasis_hotel/service/RoomService.java
@@ -0,0 +1,9 @@
+package com.oasis_hotel.oasis_hotel.service;
+
+import com.oasis_hotel.oasis_hotel.dto.room.RoomRequestDTO;
+import com.oasis_hotel.oasis_hotel.dto.room.RoomResponseDTO;
+
+public class RoomService {
+    RoomResponseDTO createRoom(RoomRequestDTO dto);
+    
+}
