7efbbc891de1337461dd0a50c09acbe74a82b1fe
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-30 20:18:35 -0500

    feature/ method createHotel created into HotelController

--- src/main/java/com/oasis_hotel/oasis_hotel/controller/HotelController.java
+++ src/main/java/com/oasis_hotel/oasis_hotel/controller/HotelController.java
@@ -1,9 +1,20 @@
 package com.oasis_hotel.oasis_hotel.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oasis_hotel.oasis_hotel.service.HotelService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.oasis_hotel.oasis_hotel.dto.hotel.HotelRequestDTO;
+import com.oasis_hotel.oasis_hotel.dto.hotel.HotelResponseDTO;
+
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/v1/api/hotels")
@@ -13,4 +24,11 @@ public class HotelController {
     public HotelController(HotelService hotelService) {
         this.hotelService = hotelService;
     }
+
+    @PostMapping
+    public ResponseEntity<HotelResponseDTO> createHotel(@Valid @RequestBody HotelRequestDTO request) {
+        HotelResponseDTO newHotel = hotelService.createHotel(request);
+        return new ResponseEntity<>(newHotel, HttpStatus.CREATED);
+    }
+    
 }
