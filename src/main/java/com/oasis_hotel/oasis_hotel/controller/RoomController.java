3bf5e6ac1e4e7fc5a90ef228074808ad38e921b8
Zairex-Code <dylanyesidflorez@gmail.com>
2026-06-01 00:54:32 -0500

    feature/ getAllRooms method created into RoomServiceImpl

--- src/main/java/com/oasis_hotel/oasis_hotel/controller/RoomController.java
+++ src/main/java/com/oasis_hotel/oasis_hotel/controller/RoomController.java
@@ -10,10 +10,16 @@ import com.oasis_hotel.oasis_hotel.service.RoomService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RequiredArgsConstructor
@@ -21,6 +27,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/v1/api/rooms")
 public class RoomController {
     private final RoomService roomService;
+
+    @GetMapping
+    public ResponseEntity<Page<RoomResponseDTO>> getAllRooms(@PageableDefault(page = 0, size = 10, sort = "id") Pageable pageable) {
+        Page<RoomResponseDTO> response = roomService.getAllRooms(pageable);
+        return ResponseEntity.ok(response);
+    }
+    
     
     @PostMapping()
     public ResponseEntity<RoomResponseDTO>createRoom(@Valid @RequestBody RoomRequestDTO request) {
