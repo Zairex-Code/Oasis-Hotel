b8878c4186700c40f1f9242b2fc545c845dcada3
Zairex-Code <dylanyesidflorez@gmail.com>
2026-06-01 22:31:54 -0500

    fix/ not null problem solved into RoomUpdateRequestDTO

--- src/main/java/com/oasis_hotel/oasis_hotel/dto/room/RoomUpdateRequestDTO.java
+++ src/main/java/com/oasis_hotel/oasis_hotel/dto/room/RoomUpdateRequestDTO.java
@@ -6,26 +6,27 @@ import com.oasis_hotel.oasis_hotel.entity.enums.RoomStatus;
 import com.oasis_hotel.oasis_hotel.entity.enums.RoomType;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record RoomUpdateRequestDTO(
     @NotBlank(message="Room number cannot be empty")
-    @Size(max=20 , message="Room numeber cannot exceed more than 20 characters")
+    @Size(max=20 , message="Room number cannot exceed more than 20 characters")
     String roomNumber,
 
-    @NotBlank(message="capacity is mandatory")
+    @NotNull(message="capacity is mandatory")
     @Positive(message="capacity must be greater than zero")
     Integer capacity,
 
-    @NotBlank(message="price per night is mandatory")
+    @NotNull(message="price per night is mandatory")
     @Positive(message="price per night must be greater than 0.0")
     BigDecimal pricePerNight,
 
-    @NotBlank(message="Room type is mandatory")
+    @NotNull(message="Room type is mandatory")
     RoomType roomType,
 
-    @NotBlank(message="Room status is mandatory")
+    @NotNull(message="Room status is mandatory")
     RoomStatus roomStatus
 
 
