47281d84d31647a537b463c8aa53d999c62d5ecb
Zairex-Code <dylanyesidflorez@gmail.com>
2026-06-06 20:32:32 -0500

    feature/ ReservationResponseDTO created

--- src/main/java/com/oasis_hotel/oasis_hotel/dto/reservation/ReservationRequestDTO.java
+++ src/main/java/com/oasis_hotel/oasis_hotel/dto/reservation/ReservationRequestDTO.java
@@ -1,5 +1,28 @@
 package com.oasis_hotel.oasis_hotel.dto.reservation;
 
-public record ReservationRequestDTO() {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.oasis_hotel.oasis_hotel.entity.enums.ReservationStatus;
+
+public record ReservationRequestDTO(
+    Long reservationId,
+    Long userId,
+    String userFirstName,
+    String userLastName,
+    String email,
+    String hotelId,
+    String hotelName,
+    Long roomId,
+    String roomNumber,
+    LocalDate checkInDate,
+    LocalDate checkOutDate,
+    Integer numberOfGuests,
+    BigDecimal totalPrice,
+    ReservationStatus status,
+    LocalDateTime createdAt
+
+) {
     
 }
