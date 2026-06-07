58b1c17c61ddc0fc09844514e62e0ca213ab2ae2
Zairex-Code <dylanyesidflorez@gmail.com>
2026-06-07 00:35:22 -0500

    feature/ ReservationController created

--- src/main/java/com/oasis_hotel/oasis_hotel/controller/ReservationController.java
+++ src/main/java/com/oasis_hotel/oasis_hotel/controller/ReservationController.java
@@ -1,14 +1,17 @@
 package com.oasis_hotel.oasis_hotel.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oasis_hotel.oasis_hotel.dto.reservation.ReservationRequestDTO;
 import com.oasis_hotel.oasis_hotel.dto.reservation.ReservationResponseDTO;
 import com.oasis_hotel.oasis_hotel.service.ReservationService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 
@@ -20,10 +23,11 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @PostMapping
-    public ResponseEntity<ReservationResponseDTO> createReservation(@RequestBody String entity) {
+    public ResponseEntity<ReservationResponseDTO> createReservation(@Valid @RequestBody ReservationRequestDTO request) {
         //TODO: process POST request
+        ReservationResponseDTO reservation = reservationService.createReservation(request);
         
-        return entity;
+        return new ResponseEntity<>(reservation ,HttpStatus.CREATED);
     }
     
 }
