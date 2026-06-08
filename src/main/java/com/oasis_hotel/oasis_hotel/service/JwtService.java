d38b0966c0359fe88ded48a9f3d56e670283aa5b
Zairex-Code <dylanyesidflorez@gmail.com>
2026-06-08 02:52:56 -0500

    feature/ Token reading methods created into JwtService

--- src/main/java/com/oasis_hotel/oasis_hotel/service/JwtService.java
+++ src/main/java/com/oasis_hotel/oasis_hotel/service/JwtService.java
@@ -4,12 +4,14 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -22,6 +24,9 @@ public class JwtService {
     @Value("${security.jwt.expiration}")
     private Long jwtExpiration;
 
+    // ==========================================
+    // 1. TOKEN CREATION (The Passport)
+    // ==========================================
 
     public String generationToken(String email , String role){
         Map<String, Object> extraClaims = new HashMap<>();
@@ -29,14 +34,39 @@ public class JwtService {
         extraClaims.put("role", role);
 
         return Jwts.builder()
-            .claims(extraClaims)
-            .subject(email)
-            .issuedAt(new Date(System.currentTimeMillis()))
-            .expiration(new Date(System.currentTimeMillis()+jwtExpiration))
-            .signWith(getSignInKey(), Jwts.SIG.HS256).compact();
+            .claims(extraClaims)                            // Extra Data Payload
+            .subject(email)                                 //Token Owner (usually the email or username)
+            .issuedAt(new Date(System.currentTimeMillis())) // Exact creation date
+            .expiration(new Date(System.currentTimeMillis()+jwtExpiration)) // Expiration date
+            .signWith(getSignInKey(), Jwts.SIG.HS256)       //modern digital signature 0.12.x
+            .compact();                                     // Assemble and convert to text (string)
 
-        
     }
+
+    // ==========================================
+    // 2. TOKEN READING (The Scanner)
+    // ==========================================
+
+    public String extractUsername (String token){
+        // In JWT, the "Subject" is the token owner (in our case, the user's email)
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver ){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                        .verifyWith(getSignInKey()) // 1. Cryptographically verify using the secret key
+                        .build()                    // 2. Build the parser/scanner
+                        .parseSignedClaims(token)   // 3. Read the signed token (Formerly parseClaimsJws)
+                        .getPayload();              // 4. Extract the internal data (Formerly getBody)
+    }
+
+
+
 
     private SecretKey getSignInKey(){
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
