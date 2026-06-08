d36740f9ccc504da2329a72179413f378d801ff0
Zairex-Code <dylanyesidflorez@gmail.com>
2026-06-08 02:59:02 -0500

    feature/ Token validation method created

--- src/main/java/com/oasis_hotel/oasis_hotel/service/JwtService.java
+++ src/main/java/com/oasis_hotel/oasis_hotel/service/JwtService.java
@@ -9,6 +9,7 @@ import java.util.function.Function;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
@@ -65,6 +66,25 @@ public class JwtService {
                         .getPayload();              // 4. Extract the internal data (Formerly getBody)
     }
 
+
+    // ==========================================
+    // 3. TOKEN VALIDATION (The Inspector)
+    // ==========================================
+    /**
+     * Spring Security manages users under the UserDetails interface.
+     * The token is valid if the email matches the DB record and if it hasn't expired.
+     */
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())&& !isTokenExpired(token));
+    }
+    private boolean isTokenExpired(String token){
+        return  extractExpiration(token).before(new Date());
+    }
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
 
 
 
